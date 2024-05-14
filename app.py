a1a42cbc942b87858a1d39b1303ca1e260ed87db
Olympiah <87874970+Olympiah@users.noreply.github.com>
2024-05-14 10:53:56 +0300

    Updated some the app.py, Added screenshots and other relevant directories.🤗

--- app.py
+++ app.py
@@ -1,85 +1,56 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 11 22:34:20 2020
-
-@author: Krish Naik
-"""
-
-from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os
-import glob
-import re
-import numpy as np
-
-# Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
 from tensorflow.keras.preprocessing import image
 
-# Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+import os
+import numpy as np
+from flask import Flask,render_template,url_for,request
 from werkzeug.utils import secure_filename
 
-# from gevent.pywsgi import WSGIServer
-
-# Define a flask app
 app = Flask(__name__)
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'model_vgg19.h5'
-
-# Load your trained model
+# models/model1.h5
+MODEL_PATH='models/model1.h5'
+# loading our saved model
 model = load_model(MODEL_PATH)
 
+# defining function
+def model_predict(img_path,model):
+    img=image.load_img(img_path,target_size=(224,224))
+    # preprocessing image by converting it to an array of pixels
+    x=image.img_to_array(img)
+    # scaling the image
+    # x=x/255
+    x = np.expand_dims(x,axis=0)
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    #  Scaling
-    x = x / 255
-    x = np.expand_dims(x, axis=0)
-
-    x = preprocess_input(x)
-
+    x=preprocess_input(x)
     preds = model.predict(x)
-    preds = np.argmax(preds, axis=1)
-    if preds == 0:
-        preds = "Parasitized"
-    else:
-        preds = "Uninfected"
+    preds = np.argmax(preds,axis=1)
 
+    if preds > 0.5:
+        preds = "You are Infected with malaria"
+    else:
+        preds = "You are not infected"
     return preds
 
-
-@app.route('/', methods=['GET'])
+# Main Page
+@app.route('/',methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
-
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict',methods=['GET','POST'])
 def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
+    if request.method =='POST':
+        file = request.files['file']
 
-        # Save the file to ./uploads
+        # saving file to./uploads
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
-        f.save(file_path)
+        file_path=os.path.join(basepath,'uploads',secure_filename(file.filename))
+        file.save(file_path)
 
-        # Make prediction
-        preds = model_predict(file_path, model)
-        result = preds
+        result = model_predict(file_path,model)
         return result
     return None
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run()
