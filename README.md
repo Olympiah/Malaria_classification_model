1cdd30e1ba98d5b0b4c19574d61a06a09a11e6e7
Olympiah <87874970+Olympiah@users.noreply.github.com>
2024-05-14 11:42:22 +0300

    Minor corrections and additions

---
+++ README.md
@@ -0,0 +1,21 @@
+# MALARIA DETECTION
+
+![UI]("screenshots/UI.jpg")
+
+This is an image classification model that classifies a huma blood smear as either Parasitized or Uninfected.
+
+## Overview
+
+1. Data Loading and Preprocessing
+2. EDA
+3. Model Development
+4. Model Evaluation
+5. Saving of the model for inference
+6. App development.
+
+
+## How to run the app
+- `git clone` the repository
+- `pip install -r requirements.txt`
+- `flask --app app run` to run the development server
+
--- app.py
+++ app.py
@@ -4,32 +4,33 @@ from tensorflow.keras.preprocessing import image
 
 import os
 import numpy as np
-from flask import Flask,render_template,url_for,request
+from flask import Flask,render_template,request
 from werkzeug.utils import secure_filename
+# import PIL
 
 app = Flask(__name__)
 # models/model1.h5
-MODEL_PATH='models/model1.h5'
+MODEL_PATH ='models/model1.h5'
 # loading our saved model
 model = load_model(MODEL_PATH)
 
 # defining function
 def model_predict(img_path,model):
-    img=image.load_img(img_path,target_size=(224,224))
+    img=  image.load_img(img_path,target_size=(224,224))
     # preprocessing image by converting it to an array of pixels
-    x=image.img_to_array(img)
-    # scaling the image
-    # x=x/255
+    x = image.img_to_array(img)
+
     x = np.expand_dims(x,axis=0)
 
-    x=preprocess_input(x)
+    x = preprocess_input(x)
     preds = model.predict(x)
+    # getting the highest probability
     preds = np.argmax(preds,axis=1)
 
     if preds > 0.5:
-        preds = "You are Infected with malaria"
+        preds = "Parasitized"
     else:
-        preds = "You are not infected"
+        preds = "Uninfected"
     return preds
 
 # Main Page
@@ -44,7 +45,7 @@ def upload():
 
         # saving file to./uploads
         basepath = os.path.dirname(__file__)
-        file_path=os.path.join(basepath,'uploads',secure_filename(file.filename))
+        file_path = os.path.join(basepath,'uploads',secure_filename(file.filename))
         file.save(file_path)
 
         result = model_predict(file_path,model)
